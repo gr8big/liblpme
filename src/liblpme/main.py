e7d30f78a1156bc599b8732bb82b371580c03b9c
gr8big <grbig137@gmail.com>
2025-12-04 08:42:13 +0000

    formatting improvements

--- src/liblpme/main.py
+++ src/liblpme/main.py
@@ -17,11 +17,13 @@ async def respond_stream(f:asyncio.StreamReader):
             yield chunk
         break
 
+
 async def chunk_writer(chunks:list[bytes], to:asyncio.StreamReader):
     for i in chunks:
         to.feed_data(len(i).to_bytes(4, "little", signed=False))
         to.feed_data(i)
     to.feed_eof()
+
 
 # general classes
 
@@ -71,11 +73,14 @@ class Session:
         self.__on_teardown = []
         self.__outgoing = asyncio.Queue()
 
-    def on_expire(self, callback:typing.Callable[[typing.Self],typing.Awaitable[None]]):
+    def on_expire(
+        self,
+        callback:typing.Callable[[typing.Self],typing.Awaitable[None]]
+    ):
         """Add a listener that is called when the session expires.
         
-        The callback should take one argument, which is the issuing `Session`.
-        It should return an `Awaitable`.
+        The callback should take one argument, which is the issuing
+        `Session`. It should return an `Awaitable`.
         
         Expiry callbacks are called when the expiry timer runs out.
         After all calls are completed, `self.teardown()` is scheduled.
@@ -84,15 +89,17 @@ class Session:
         self.__on_expire.append(callback)
         return callback
     
-    def on_teardown(self, callback:typing.Callable[[typing.Self],typing.Awaitable[None]]):
+    def on_teardown(
+        self,
+        callback:typing.Callable[[typing.Self],typing.Awaitable[None]]
+    ):
         """Add a listener that is called when the session expires.
         
-        The callback should take one argument, which is the issuing `Session`.
-        It should return an `Awaitable`.
+        The callback should take one argument, which is the issuing
+        `Session`. It should return an `Awaitable`.
         
         Expiry callbacks are called **after** the session is torn down.
-        This is either after it expires, or when it is manually
-        closed.
+        This is either after it expires, or when it is manually closed.
         """
 
         self.__on_teardown.append(callback)
@@ -155,8 +162,8 @@ class Session:
     async def teardown(self):
         """Shut down the session.
 
-        This clears the token, all expiry hooks and cancels the
-        expiry loop task created at construction.
+        This clears the token, all expiry hooks and cancels the expiry
+        loop task created at construction.
         """
 
         self.__on_expire.clear()
@@ -171,8 +178,8 @@ class Session:
             await i(self)
 
     async def get_user_token(self) -> str:
-        """Get the token as a string that can be used as a raw
-        HTTP header.
+        """Get the token as a string that can be used as a raw HTTP
+        header.
         """
 
         return str(self.__token_str, "utf8")
@@ -181,9 +188,9 @@ class Session:
     async def long_poll(self, max_ttl:float=55.0) -> list[bytes]:
         """Long-poll the outgoing message queue.
         
-        The `max_ttl` is used as a hard limit on how long a
-        single poll is allowed to take. If this limit is
-        reached, an empty list is returned.
+        The `max_ttl` is used as a hard limit on how long a single poll
+        is allowed to take. If this limit is reached, an empty list is
+        returned.
         """
 
         res = []
@@ -199,15 +206,13 @@ class Session:
         return res
 
     async def push(self, message:bytes):
-        """Push a message to be sent at the next long-poll
-        cycle.
+        """Push a message to be sent at the next long-poll cycle.
 
         For general use, use `Session.run`.
 
-        Messages are inserted into the outgoing queue. As
-        such, if the session has been shut down already, this
-        will fail, as the queue will have also been shut
-        down.
+        Messages are inserted into the outgoing queue. As such, if the
+        session has been shut down already, this will fail, as the queue
+        will have also been shut down.
         """
 
         self.__outgoing.put_nowait(message)
@@ -215,25 +220,24 @@ class Session:
     async def run(self, command:str, body:bytes):
         """Push a message to the client to run a command.
 
-        This uses `Session.push` internally to send a
-        formatted message.
+        This uses `Session.push` internally to send a formatted message.
         """
 
         await self.push(bytes(command, "utf8") + b"\0" + body)
 
+
 class SessionManager:
-    """Utility to manage `Session` objects, using session IDs as
-    a reference.
+    """Utility to manage `Session` objects, using session IDs as a
+    reference.
     """
 
     def __init__(self, key:str, hasher:PasswordHasher|None=None):
         """Session manager constructor.
 
-        The `key` must be an Argon2id-encoded password hash,
-        encoded using the provided hasher.
+        The `key` must be an Argon2id-encoded password hash, encoded
+        using the provided hasher.
 
-        If no hasher is provided, the default Argon2 arguments
-        are used.
+        If no hasher is provided, the default Argon2 arguments are used.
         """
 
         if hasher is None:
@@ -247,11 +251,11 @@ class SessionManager:
     def get_session(self, id:int) -> Session|None:
         """Get a session, using a user-issued ID as a key.
 
-        If the session exists, it is returned, otherwise `None`
-        is returned.
+        If the session exists, it is returned, otherwise `None` is
+        returned.
 
-        Note, this does **not** perform validation. You still
-        need to validate the session after it is returned.
+        Note, this does **not** perform validation. You still need to
+        validate the session after it is returned.
         """
 
         if id in self.__sessions:
@@ -261,9 +265,8 @@ class SessionManager:
     async def start_session(self, lifetime:float=30) -> Session:
         """Start a session with the given lifetime.
 
-        This constructs a session (using `Session(lifetime)`),
-        adds it to the manager's internal dictionary, and
-        returns it.
+        This constructs a session (using `Session(lifetime)`), adds it
+        to the manager's internal dictionary, and returns it.
         """
 
         id = self.__id_prog
@@ -275,20 +278,22 @@ class SessionManager:
         return ses
 
     async def authenticate(self, key:str, lifetime:float=30) -> Session:
-        """Validate a user-issued API key against the stored
-        hash.
+        """Validate a user-issued API key against the stored hash.
 
         `lifetime` is used as the `lifetime` argument in
-        `self.start_session`, which is called if the key is
-        valid.
+        `self.start_session`, which is called if the key is valid.
 
         This raises an error if the key is not valid. See the
-        documentation for `argon2.PasswordHasher` for details.
-        Should a quantum bit-flip occur, this may also raise a
-        `RuntimeError` on fail.
+        documentation for `argon2.PasswordHasher` for details. Should a
+        quantum bit-flip occur, this may also raise a `RuntimeError` on
+        fail.
         """
 
-        if await asyncio.to_thread(self.__hasher.verify, self.__key, key) is True:
+        if await asyncio.to_thread(
+            self.__hasher.verify,
+            self.__key,
+            key
+        ) is True:
             return await self.start_session(lifetime)
         
         # literally impossible
@@ -296,6 +301,7 @@ class SessionManager:
 
     async def __teardown(self, ses:Session):
         del self.__sessions[ses.id]
+
 
 # main class
 
@@ -313,13 +319,14 @@ class LPMEEndpointApi:
             hasher:PasswordHasher|None=None,
             lifetime:float=30
     ):
-        """Construct an `LPMEEndpointApi` around an existing
-        app.
+        """Construct an `LPMEEndpointApi` around an existing app.
         """
 
         if manager is None:
             if api_key is None:
-                raise ValueError("API key must be set when manager is not")
+                raise ValueError(
+                    "API key must be set when manager is not"
+                )
             
             manager = SessionManager(api_key, hasher)
 
@@ -328,17 +335,19 @@ class LPMEEndpointApi:
         self.__manager = manager
         self.__lifetime = lifetime
 
-        app.route(self.__base_endpoint, methods=["POST"])(self.__hndl_auth)
+        app.route(
+            self.__base_endpoint,
+            methods=["POST"]
+        )(self.__hndl_auth)
         self.event("/liblpme/shutdown")(self.__hndl_shutdown)
         self.event("/liblpme/longpoll")(self.__hndl_longpoll)
 
     def event(self, endpoint:str):
         """Add an event listener.
 
-        The handler for an event is identical to a standard
-        Quart request handler. However, the first argument
-        passed will always be the `Session` object of the
-        client executing the command.
+        The handler for an event is identical to a standard Quart
+        request handler. However, the first argument passed will always
+        be the `Session` object of the client executing the command.
         """
 
         if not endpoint.startswith("/"):
@@ -346,8 +355,15 @@ class LPMEEndpointApi:
         
         def wrapper(callback:typing.Callable[...,typing.Awaitable]):
             async def handler(*args, **kwargs):
-                ses_tk = request.headers.get("X-LPME-Session", "")
-                ses_id = request.headers.get("X-LPME-Session-Id", -1, int)
+                ses_tk = request.headers.get(
+                    "X-LPME-Session",
+                    ""
+                )
+                ses_id = request.headers.get(
+                    "X-LPME-Session-Id",
+                    -1,
+                    int
+                )
                 session = self.__manager.get_session(ses_id)
 
                 if session is None:
@@ -362,7 +378,10 @@ class LPMEEndpointApi:
                         else:
                             res = make_response(res)
 
-                    res.headers.set("X-LPME-Server-Id", session.unique_id)
+                    res.headers.set(
+                        "X-LPME-Server-Id",
+                        session.unique_id
+                    )
                     return res
                 else:
                     return "Forbidden", 403
@@ -383,7 +402,10 @@ class LPMEEndpointApi:
         api_key = request.headers.get("X-LPME-Token", "")
 
         try:
-            ses = await self.__manager.authenticate(api_key, self.__lifetime)
+            ses = await self.__manager.authenticate(
+                api_key,
+                self.__lifetime
+            )
 
             response = Response("", 200, mimetype="text/plain")
             response.headers.set("X-LPME-Session-Id", str(ses.id))
@@ -402,6 +424,10 @@ class LPMEEndpointApi:
         chunks = await ses.long_poll()
         asyncio.create_task(chunk_writer(chunks, f))
 
-        res = Response(respond_stream(f), 200, mimetype="application/x-lpme-chunks")
+        res = Response(
+            respond_stream(f),
+            200,
+            mimetype="application/x-lpme-chunks"
+        )
         res.headers.set("X-LPME-Chunk-Count", str(len(chunks)))
         return res
